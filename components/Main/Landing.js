3580518910bf9fdd48ad67d61a587c7d8e028a5b
Shruti <lucky@Shrutis-MacBook-Air.local>
2022-09-09 00:58:56 +0530

    basic next setup

---
+++ components/Main/Landing.js
@@ -0,0 +1,17 @@
+import {
+  faTwitter,
+  faInstagram,
+  faDiscord,
+} from "@fortawesome/free-brands-svg-icons";
+import React from "react";
+import Image from "next/image";
+
+const Landing = () => {
+  return (
+    <div>
+      <Image src="/background.png" alt="bg" width="400" height="400" />
+    </div>
+  );
+};
+
+export default Landing;
